67271a53747ea3313edfddc3566dec31e8862947
Chesnokov M.A <chesnokov_ma@mail.ru>
2024-01-30 23:15:11 +0500

    commit 2 seminar2

--- seminar2/task9.py
+++ seminar2/task9.py
@@ -7,27 +7,47 @@ from flask import Flask, render_template, request, url_for, make_response, redir
 # с данными пользователя и произведено перенаправление на страницу ввода имени и электронной почты.
 
 app = Flask(__name__)
-app.secret_key = 'a18dd37767c54faf75ee2fb5298aad8cc753eba70d8b42efca0aae1d1dea9434'
+# app.secret_key = 'a18dd37767c54faf75ee2fb5298aad8cc753eba70d8b42efca0aae1d1dea9434'
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
+@app.route('/')
+def index():
     content = {'title': 'Страница входа',
                'name_form': 'Введите имя и электронную почту'}
+    return render_template('send_form.html', **content)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
         context = {'title': 'Успешный вход',
-                    'name': request.form.get('username').capitalize()}
+                   'text': 'Добро пожаловать ',
+                   'name': request.form.get('username').capitalize(),
+                   }
         response = make_response(render_template('welcome.html', **context))
         response.set_cookie('username', request.form.get('username'))
         response.set_cookie('email', request.form.get('email'))
         return response
-    return render_template('send_form.html', **content)
+    return redirect(url_for('index'))
 
-@app.post('/logout')
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    # request.cookies.pop('username')
-    # request.cookies.pop('email')
-    print(request.cookies)
-    return redirect(url_for('login'))
+    if request.method == 'POST':
+        content = {'title': 'Страница входа',
+                   'name_form': 'Введите имя и электронную почту',
+                   }
+        response = make_response(render_template('send_form.html', **content))
+        response.delete_cookie('username')
+        response.delete_cookie('email')
+        return response
+    else:
+        if request.cookies.get('username'):
+            context = {'title': 'Успешный вход',
+                        'text': 'Добро пожаловать ',
+                        'name': request.cookies.get('username').capitalize(),
+                        }
+            response = make_response(render_template('welcome.html', **context))
+            return response
+        else:
+            return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
